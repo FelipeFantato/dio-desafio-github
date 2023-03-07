4d102939e3b44d4c00a5231cc8b9e991d30fed45
Felipe Fantato <67489901+FelipeFantato@users.noreply.github.com>
2023-03-07 10:59:30 -0300

    Desenvolvimento de aulas em lógica de programação em Java

--- Projetos/Java-Basico/java-anatomia-classes/src/MinhaClasse.java
+++ Projetos/Java-Basico/java-anatomia-classes/src/MinhaClasse.java
@@ -16,9 +16,29 @@ public class MinhaClasse {
 
         String nomecompleto = nomeCompleto(primeiroNome, segundoNome);
         System.out.println(nomecompleto);
+        int numero = 3;
+        qualNumero(numero);
+
+
     }
     public static String nomeCompleto (String primeiroNome, String segundoNome){
         return primeiroNome.concat(" ").concat(segundoNome);
     }
 
+    private static void qualNumero(int numero){
+        switch (numero) {
+            case 1:
+            case 2:
+            case 3:
+                System.out.println("Numero entre 1 a 3");
+                break;
+            case 4:
+                System.out.println("numero 4");
+                break;
+                
+            default:
+                System.out.println("Valor indefinido!");
+                break;
+        }
+    }
 }
