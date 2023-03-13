2430bdeab49a12958b082c4e60aaa5f422397d85
Felipe Fantato <67489901+FelipeFantato@users.noreply.github.com>
2023-03-13 11:37:34 -0300

    Aula de Arvores Binarias

--- Projetos/Arvore-Binaria/arvorebinaria/src/main/java/one/digitalinobattion/ArvoreBinaria.java
+++ Projetos/Arvore-Binaria/arvorebinaria/src/main/java/one/digitalinobattion/ArvoreBinaria.java
@@ -1,2 +1,10 @@
-package one.digitalinobattion;public class ArvoreBinaria {
+package one.digitalinobattion;
+
+public class ArvoreBinaria<T extends Comparable<T>> {
+
+    private BinNo<T> raiz;
+
+    public ArvoreBinaria(){
+        this.raiz = null;
+    }
 }
