13c96359d55942d5f6ddc9324fceea3a48d1ed16
Felipe Fantato <67489901+FelipeFantato@users.noreply.github.com>
2023-03-13 15:08:57 -0300

    Exercicios

---
+++ Projetos/ExerciciosPraticos/src/main/java/org/example/Calculadora.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Calculadora {
+
+    public static int calcularSoma(int n1, int n2){
+        return n1 + n2;
+
+    }
+    public static int calcularSub(int n1, int n2){
+        return n1 - n2;
+    }
+    public static float calcularDiv(int n1, int n2){
+        return n1 / n2;
+    }
+    public static int calcularMulti(int n1, int n2){
+        return n1 * n2;
+    }
+
+}
---
+++ Projetos/ExerciciosPraticos/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println(Calculadora.calcularSoma(2,2));
+        System.out.println(Calculadora.calcularSub(2,2));
+        System.out.println(Calculadora.calcularDiv(2,2));
+        System.out.println(Calculadora.calcularMulti(2,2));
+
+
+
+    }
+
+
+}
